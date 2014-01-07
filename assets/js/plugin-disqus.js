ade4d445198570c480d32dcec4830341061405bf
Mario Casciaro <mariocasciaro@gmail.com>
2014-01-07 20:03:03 +0000

    Uses new Hadron 0.2 plugin hooks

--- assets/js/plugin-disqus.js
+++
@@ -1,8 +0,0 @@
-
-(function(shatter) {
-  shatter.use('saveSettingsCtrl', 'controllerImpl', function($scope) {
-    $scope.saveDisqus = function(settings, invalid) {
-      return $scope.save('disqusForumShortname', settings, invalid);
-    };
-  });
-})(window.shatter);
